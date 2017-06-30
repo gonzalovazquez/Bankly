f2042ec9b1a53a4788d966ea06dce4b723f00f2d
gonzalovazquez <gonzalovazquez010@gmail.com>
2017-06-29 23:46:52 -0400

    Feat: Wishlist WIP component

--- App/Components/ActionComponents/Settings.js
+++ App/Components/ActionComponents/Settings.js
@@ -2,7 +2,40 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  StyleSheet,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    top: 20,
+    height: '100%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    shadowColor: '#B9B9B9',
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+  },
+  header: {
+    flexDirection: 'row',
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  textHeader: {
+    fontSize: 18,
+    fontWeight: '600',
+    width: '40%',
+    paddingLeft: 20,
+    paddingTop: 10,
+  },
+});
 
 class Settings extends Component {
   static navigationOptions = {
@@ -10,8 +43,10 @@ class Settings extends Component {
   };
   render() {
     return (
-      <View>
-        <Text>Settings</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.textHeader}>Settings</Text>
+        </View>
       </View>
     );
   }
--- App/Components/ActionComponents/Wishlist.js
+++ App/Components/ActionComponents/Wishlist.js
@@ -2,16 +2,126 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  StyleSheet,
+  ScrollView,
+  Button,
+  Modal,
+  TouchableHighlight
 } from 'react-native';
+
+import wishlist from '../../mock/wishlist.json';
+
+const styles = StyleSheet.create({
+  container: {
+    top: 20,
+    height: '100%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    shadowColor: '#B9B9B9',
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+  },
+  header: {
+    flexDirection: 'row',
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  textHeader: {
+    fontSize: 18,
+    fontWeight: '600',
+    width: '40%',
+    paddingLeft: 20,
+    paddingTop: 10,
+  },
+  wishlistBody: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  rowChore: {
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  plusButton: {
+    borderStyle: 'solid',
+    borderRadius: 5,
+    borderColor: '#000'
+  },
+  modalContainer: {
+
+  },
+});
+
+const WishlistModal = (props) => (
+  <Modal
+    animationType={"slide"}
+    transparent={false}
+    visible={props.visible}
+    >
+    <View style={styles.modalContainer}>
+      <Text>
+        Hello World
+      </Text>
+      <TouchableHighlight onPress={() => {
+        props.setModalVisible(!props.visible)
+      }}>
+        <Text>Add item</Text>
+      </TouchableHighlight>
+    </View>
+  </Modal>
+);
 
 class Wishlist extends Component {
   static navigationOptions = {
     title: 'Wishlist',
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      wishlist,
+      modalVisible: false,
+    };
+  }
   render() {
+    const onPressLearnMore = () => {
+      this.setState({ modalVisible: true });
+    };
+    const setModalVisible = (visible) => {
+      console.log("^_____^");
+      console.log(visible);
+      console.log("-______-");
+      this.setState({ modalVisible: visible });
+    };
     return (
-      <View>
-        <Text>Wishlist</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.textHeader}>Wishlist</Text>
+        </View>
+        <WishlistModal
+          visible={this.state.modalVisible}
+          setModalVisible={setModalVisible}
+          />
+        <ScrollView style={styles.wishlistBody}>
+          {this.state.wishlist.map((item, index) => (
+            <View key={index} style={styles.rowChore}>
+              <Text>{item.name} | ${item.cost}</Text>
+            </View>
+          ))}
+          <Button
+            style={styles.plusButton}
+            onPress={onPressLearnMore}
+            title="+"
+            color="#c3c3c3"
+            accessibilityLabel="Add Wishlist"
+          />
+        </ScrollView>
       </View>
     );
   }
