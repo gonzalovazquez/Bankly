138724532d454e446d6f069233bd1cb06ff22e9f
gonzalovazquez <gonzalovazquez010@gmail.com>
2017-09-10 14:24:09 -0400

    Feat: Updated chore reducer and actions

--- App/actions/chores.js
+++ App/actions/chores.js
@@ -1,62 +1,84 @@
+import chores from '../mock/chores.json';
+
+/**
+ * Export object of error state
+ * @param {boolean} bool - Error state
+ */
+export const choresHasErrored = (bool) => ({
+  type: 'CHORES_HAS_ERRORED',
+  hasErrored: bool,
+});
+
+/**
+ * Export object of loading state
+ * @param {boolean} bool - Loading state 
+ */
+export const choresIsLoading = (bool) => ({
+  type: 'CHORES_IS_LOADING',
+  isLoading: bool,
+});
+
+/**
+ * Export object if chore is successful
+ * @param {object} items - new list of chores
+ */
+export const choresIsSuccessful = (items) => ({
+  type: 'CHORES_IS_SUCCESSFUL',
+  items,
+});
+
 /**
  * Export object with new chore
  * @param {object} items - new chore
  */
-export function createNewChore(items) {
-  return {
-    type: 'CREATE_NEW_CHORE',
-    items,
-  };
-}
+export const createNewChore = (items) => ({
+  type: 'CREATE_NEW_CHORE',
+  items,
+});
+
 
 /**
  * Export object of deleted ID
  * @param {number} id  - Chore ID
  */
-export function deleteChore(id) {
-  return {
-    type: 'DELETE_CHORE',
-    id,
-  };
-}
+export const deleteChore = (id) => ({
+  type: 'DELETE_CHORE',
+  id,
+});
 
 /**
  * Export object of updated chore
  * @param {numbere} id - Chore ID 
  */
-export function updateCompleteChore(id) {
-  return {
-    type: 'UPDATE_COMPLETE_CHORE',
-    id,
-  };
+export const updateCompleteChore = (id) => ({
+  type: 'UPDATE_COMPLETE_CHORE',
+  id,
+});
+
+export const fetchChores = () => (dispatch) => {
+  dispatch(choresIsSuccessful(chores));
 }
 
 /**
  * Create new chore
  * @param {object} chore - New Chore
  */
-export function newChore(chore) {
-  return (dispatch) => {
-    dispatch(createNewChore(chore));
-  };
+export const newChore = (chore) => (dispatch) => {
+  dispatch(createNewChore(chore));
 }
 
 /**
  * Delete one chore
  * @param {number} id - ID of Chore
  */
-export function deleteOneChore(id) {
-  return (dispatch) => {
-    dispatch(deleteChore(id));
-  };
+export const deleteOneChore = (id) => (dispatch) => {
+  dispatch(deleteChore(id));
 }
 
 /**
  * Update one chore to flag as completed
  * @param {number} id - ID of Chore 
  */
-export function updateChore(id) {
-  return (dispatch) => {
-    dispatch(updateCompleteChore(id));
-  };
+export const updateChore = (id) => (dispatch) => {
+  dispatch(updateCompleteChore(id));
 }
--- App/reducers/chores.js
+++ App/reducers/chores.js
@@ -1,5 +1,8 @@
 const initialState = {
   chores: [],
+  hasErrored: false,
+  isLoading: false,
+  isSuccessful: false,
 };
 
 /**
@@ -10,20 +13,23 @@ const initialState = {
 export default function chores(state = initialState, action) {
   const newState = { ...state };
   switch (action.type) {
+    case 'CHORES_IS_SUCCESSFUL':
+      newState.chores = action.items;
+      return newState;
     case 'CREATE_NEW_CHORE':
-    newState.chores.push(action.items);
-    return newState;
-  case 'DELETE_CHORE':
-    newState.chores.pop(action.id);
-    return newState;
-  case 'UPDATE_COMPLETE_CHORE':
-    newState.chores.map((chore) => {
-      if (chore.id == action.id) {
-        chore.completed = !chore.completed
-      }
-    });
-    return newState;
-  default:
-    return newState;
+      newState.chores.push(action.items);
+      return newState;
+    case 'DELETE_CHORE':
+      newState.chores.pop(action.id);
+      return newState;
+    case 'UPDATE_COMPLETE_CHORE':
+      newState.chores.map((chore) => {
+        if (chore.id == action.id) {
+          chore.completed = !chore.completed
+        }
+      });
+      return newState;
+    default:
+      return newState;
   }
 }
